e59eadf76b5414b07610c83cd84870605e4f2609
is7012bechmana <105136809+bechmana@users.noreply.github.com>
2023-11-17 00:55:35 -0500

    Visitor Center Page and link from States page from the ParkCode
    Visitor center page front and back.
    Link on the Park Code on the states that will link to the Visitor Center page with the code
    Search option on the Visitor Center Page for specific codes

---
+++ ParkPlanner2/Pages/Visitor Center.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ParkPlannerVisitor;
+
+namespace ParkPlanner2.Pages
+{
+    public class Visitor_CenterModel : PageModel
+    {
+        public string ParkCode { get; set; }
+        static readonly HttpClient client = new HttpClient();
+        public void OnGet(string query)
+        {
+            var task = client.GetAsync("https://developer.nps.gov/api/v1/visitorcenters?api_key=mLBONbm3NZfawfBoS0w4bXT3yyJ1nBpLhqh6o0Au\r\n");
+            HttpResponseMessage result = task.Result;
+            List<Datum> VC = new List<Datum>();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                VC = VisitorCenter.FromJson(jsonString).Data;
+                if (!string.IsNullOrWhiteSpace(query))
+                {
+                    var AllVisitorCenter = VC.ToList();
+                    var VisitorCenter = AllVisitorCenter.FindAll(x => string.Equals(x.ParkCode, query, StringComparison.OrdinalIgnoreCase)).ToList();
+
+                    if (VisitorCenter != null && VisitorCenter.Count > 0)
+                    {
+
+                        ViewData["VisitorCenterList"] = VisitorCenter;
+                    }
+                    else
+                    {
+                        ViewData["VisitorCenterList"] = null;
+                    }
+                }
+                else
+                {
+                    ViewData["VisitorCentert"] = null;
+                }
+                ParkCode = query;
+            }
+        }
+    }
+}
